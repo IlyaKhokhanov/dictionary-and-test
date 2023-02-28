9e8f6331327c2e2929e98114865d654866021994
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-02-28 22:12:30 +0300

    first refactoring 2 page

--- src/utils/webSpeech.js
+++
@@ -1,9 +0,0 @@
-const { speechSynthesis } = window;
-
-export default function webSpeech(value) {
-  if (!speechSynthesis.speaking) {
-    const ssu = new SpeechSynthesisUtterance(value);
-    ssu.lang = "en-US";
-    speechSynthesis.speak(ssu);
-  }
-}
