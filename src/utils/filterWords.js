2f20a662f2214dbc98580be1de1ea820e787b4ea
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-03-05 00:08:05 +0300

    make refactoring 2 page

--- src/utils/filterWords.js
+++ src/utils/filterWords.js
@@ -1,12 +1,13 @@
 export function filteredWords(state, data) {
   const res = [];
-  for (let sb in state) {
-    if (!state[sb].length) {
+  const objState = Object.fromEntries(state);
+  for (let sb in objState) {
+    if (!objState[sb].length) {
       for (let downloadedModules in data) {
         data[downloadedModules].forEach((objWord) => res.push(objWord));
       }
     }
-    state[sb].forEach((modulesState) => {
+    objState[sb].forEach((modulesState) => {
       for (let downloadedModules in data) {
         if (downloadedModules === modulesState) {
           data[downloadedModules].forEach((objWord) => res.push(objWord));
